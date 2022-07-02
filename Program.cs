52008be6602e5ce8656b5d677963dedcaced2bb3
Krivyh.a <Krivyh.a@medindex.ru>
2022-07-02 14:50:52 +0300

    Swagger added

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSwaggerGen();
 var mvcBuilder = builder.Services.AddControllers();
 mvcBuilder.AddControllersAsServices();
 
 var app = builder.Build();
 app.MapControllers();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 app.Run();
